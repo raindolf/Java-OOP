b91dc349b260acbf97047104b4971de772f8b47a
Raindolf Owusu <iraindolf@yahoo.com>
2014-05-01 13:19:03 +0000

    percent calculation

---
+++ percentage.java
@@ -0,0 +1,25 @@
+
+public class percentage {
+
+	public static void main (String [] args)
+	
+	{
+		
+		double num1 = 70;
+		
+		double num2 = 1800;
+		
+		double result;
+		
+		result = ( num1 / 100 ) * num2 ;
+ 		
+		System.out.println ( num1 + " percent of " + num2 + " is " + result ); 
+		
+	
+		
+		
+		
+		
+	}
+	
+}
