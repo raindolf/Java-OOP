060c2dffed4e6120a6aa3d68f891ce3e3e1d4c80
Raindolf Owusu <iraindolf@yahoo.com>
2014-05-10 15:35:37 +0000

    Ann and rain push

---
+++ marriage.java
@@ -0,0 +1,40 @@
+
+public class marriage {
+
+	
+
+	
+	public static void main ( String [] args)
+	
+	{
+		
+		int ann = 26;
+		int rain = 24;
+		String adjoa = "Ann has a big head";
+		
+		int result;
+		
+		if ( rain >= ann)
+			
+		{
+			System.out.println ("We are not getting married" );
+			
+			
+		}
+		
+		else if ( ann >= rain)
+
+		{
+			System.out.println("We are getting married");
+		}
+		
+		
+		else 
+			
+		{
+			System.out.println("Try again");
+			
+		}
+		
+	}
+}
